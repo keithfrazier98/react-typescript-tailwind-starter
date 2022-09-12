f3f15e48f1e62c36a339743ca4bc045b52246ad6
Keith Frazier <keithers98@gmail.com>
2022-09-12 16:04:23 -0700

    updated dependency versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
-// tailwind.config.js
-// in this file we can add the customized colors tailwind provides.
-
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
-   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode:'class',
-    theme: {
-      extend: {},
-      colors: {
-      transparent: 'transparent',
-      current: 'currentColor', 
-      primary: colors.primary
-    }
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      // colors: {
+      //   transparent: "transparent",
+      //   current: "currentColor",
+      //   primary: colors.primary,
+      // },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [
-   ],
-  }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
